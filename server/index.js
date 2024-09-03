9a7f10c48c450321fc12f845fd624f0ef18de1a1
Ani no <bryanlomerioanino@gmail.com>
2024-09-03 23:39:39 +0800

    test

--- server/index.js
+++ server/index.js
@@ -19,8 +19,8 @@ const io = new Server(server, {
 const mongoURI = process.env.MONGO_URI;
 
 mongoose.connect(mongoURI, {
-    useNewUrlParser: true,  // Safely remove if using MongoDB driver v4.0.0 or later
-    useUnifiedTopology: true // Safely remove if using MongoDB driver v4.0.0 or later
+    useNewUrlParser: true,  
+    useUnifiedTopology: true 
 })
 .then(() => console.log('MongoDB connected'))
 .catch(err => console.error('MongoDB connection error:', err));
